2a6d9aace5e3bdc8b2937201c05459d90fe403d1
EliteHacker228 <mchappycraft@yandex.ru>
2022-05-29 21:54:15 +0500

    completed tasks 1, 2, 4

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Project;
+use App\Type\ProjectType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class ProjectController extends AbstractController
+{
+    /**
+     *
+     * @Route("/projects/create", name="project_create")
+     * @param Request $request
+     * @return Response
+     */
+    public function create(Request $request): Response
+    {
+        $project = new Project();
+        $form = $this->createForm(ProjectType::class, $project);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $project->setOwner($this->getUser());
+
+            $this->getDoctrine()->getManager()->persist($project);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('project_list');
+        }
+
+        return $this->render("project/create.html.twig", [
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    /**
+     * @Route("/projects", name = "project_list")
+     * @return Response
+     */
+    public function list(Request $request): Response
+    {
+        $projects = $this->getDoctrine()->getManager()
+            ->getRepository(Project::class)
+            ->findAll();
+        return $this->render('project/list.html.twig', [
+            'projects' => $projects,
+        ]);
+    }
+}
---
+++ src/Entity/Project.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjectRepository::class)
+ */
+class Project{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     * @Assert\NotBlank
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 5
+     * )
+     * @var
+     */
+    protected $token; //5 latin symbols
+
+    /**
+     * @param mixed $name
+     */
+    public function setToken($token): void
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @var
+     */
+    protected $name;
+
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="user")
+     * @var User
+     */
+    protected $owner;
+
+    /**
+     * Set task Author
+     * @param User|null $author
+     * @return void
+     */
+    public function setOwner(UserInterface $owner = null)
+    {
+        $this->owner = $owner;
+    }
+
+
+    /**
+     * Return task author
+     * @return User|null
+     */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+}
